59cb4d77dcec293f94f1c34f061031b255f9eca8
timmer98 <38386858+timmer98@users.noreply.github.com>
2022-06-29 20:45:37 +0200

    Implemented solution for day 4 star 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,12 +1,33 @@
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use colored::Colorize;
+
+const TEST_INPUT: &str = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
 
 fn main() {
     let content = read_file();
     let draws = read_draw_numbers(&content);
     let mut boards = read_boards(&content);
-    let score = play(&mut boards, &draws);
+    let score = find_last_winning_board(&mut boards, &draws);
 
     println!("{}", score);
 }
@@ -48,6 +69,29 @@ fn play(boards: &mut Vec<Board>, draws: &Vec<i32>) -> i32 {
             }
         }
 
+    }
+
+    return 0;
+}
+
+fn find_last_winning_board(boards: &mut Vec<Board>, draws: &Vec<i32>) -> i32{
+    for i in 0..draws.len() {
+        let draw = draws[i];
+        let mut j = 0;
+
+        while j < boards.len() {
+            let win = boards[j].check_number(draw);
+
+            if win {
+                if boards.len() == 1 {
+                    return boards[j].calculate_score(draw);
+                }
+
+                 boards.remove(j);
+            } else {
+                j += 1;
+            }
+        }
     }
 
     return 0;
@@ -103,6 +147,18 @@ impl Board {
             }
         }
 
+        for i in 0..self.number_matrix.len() {
+            for j in 0..self.number_matrix[i].len() {
+                if self.bitvector[i][j] {
+                    print!("{0: <5} ", format!("+{}+", self.number_matrix[i][j]).green());
+                } else {
+                    print!("{0: <5} ", self.number_matrix[i][j])
+                }
+            }
+            println!();
+        }
+        println!();
+
         return self.evaluate();
     }
 
@@ -127,35 +183,9 @@ impl Board {
             }
         }
 
-        // Check diagonal
-        if self.bitvector[0][0] && self.bitvector[1][1] && self.bitvector[2][2] && self.bitvector[3][3] && self.bitvector[4][4] {
-            return true;
-        }
-
         return false;
     }
 }
-
-#[cfg(test)]
-const TEST_INPUT: &str = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7";
 
 #[cfg(test)]
 mod tests_star1 {
@@ -168,5 +198,14 @@ mod tests_star1 {
         let score = play(&mut boards, &draws);
 
         assert_eq!(score, 4512);
+    }
+
+    #[test]
+    fn test2() {
+        let draws = read_draw_numbers(TEST_INPUT);
+        let mut boards = read_boards(TEST_INPUT);
+        let score = find_last_winning_board(&mut boards, &draws);
+
+        assert_eq!(score, 1924);
     }
 }
